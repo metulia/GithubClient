6468c39d2be93bba8077a7ba2394d8de0c05b70b
Yuliia Stepkina <metulia@mail.ru>
2023-06-08 18:08:48 +0300

    Добавила в MainActivity.kt ссылку на MainPresenter, listener для кнопок и реализацию интерфейса MainView

--- app/src/main/java/com/example/githubclient/MainActivity.kt
+++ app/src/main/java/com/example/githubclient/MainActivity.kt
@@ -2,16 +2,36 @@ package com.example.githubclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.githubclient.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
 
     private var binding: ActivityMainBinding? = null
+
+    private val presenter = MainPresenter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding?.root)
+
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.id)
+        }
+
+        binding?.btnCounter1?.setOnClickListener(listener)
+        binding?.btnCounter2?.setOnClickListener(listener)
+        binding?.btnCounter3?.setOnClickListener(listener)
+    }
+
+    override fun setButtonText(index: Int, text: String) {
+        when (index) {
+            0 -> binding?.btnCounter1?.text = text
+            1 -> binding?.btnCounter2?.text = text
+            2 -> binding?.btnCounter3?.text = text
+        }
     }
 }
